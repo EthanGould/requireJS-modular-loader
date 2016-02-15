e62a9a38cf8aa754fc06690b76872ebd32166068
EthanGould <ethan.g.907@gmail.com>
2016-02-15 14:08:06 -0500

    set word of day to be consistent

--- scripts/base.js
+++ scripts/base.js
@@ -1,4 +1,6 @@
 var $ = require([ './scripts/lib/jquery.min.js' ]);
 require([ './scripts/modules/friendly-jester.js' ]);
 require([ './scripts/modules/wotd.js' ]);
+// require([ './scripts/modules/meme.js' ]);
+
 
--- scripts/modules/wotd.js
+++ scripts/modules/wotd.js
@@ -34,12 +34,19 @@ require([], function() {
 	};
 
 	module.getWord = function() {
-		var year = 2015,
-				month = Math.floor( Math.random() * 12 + 1 ),
-				day = Math.floor( Math.random() * 28 + 1 ),
-				date = year + '-' + month + '-' + day,
+		//		year = 2015,
+		// 		month = Math.floor( Math.random() * 12 + 1 ),
+		// 		day = Math.floor( Math.random() * 28 + 1 ),
+		// 		date = year + '-' + month + '-' + day,
+
+		var date = new Date(),
+				year = date.getFullYear(),
+				month = date.getMonth() + 1,
+				day = date.getDate();
+				today = year +'-'+ month +'-'+ day;
+
 				key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-				url = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + date + '&api_key=' + key;
+				url = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + today + '&api_key=' + key;
 		module.$loading.show();
 		module.$wordDetails.hide();
 		$.get( url, module.getDefinition )
