5007c146d05b441c39ccc90b1e41577d5a8eab10
EthanGould <ethan.g.907@gmail.com>
2016-02-14 22:00:27 -0500

    updated front end for word of the day

--- scripts/modules/wotd.js
+++ scripts/modules/wotd.js
@@ -10,7 +10,8 @@ require([], function() {
 		$word: $( '.js-word' ),
 		$definition: $( '.js-definition' ),
 		$note: $( '.js-note' ),
-		$example: $( '.js-example' )
+		$example: $( '.js-example' ),
+		$partOfSpeech: $( '.js-speech-part' )
 	};
 
 	module.init = function() {
@@ -18,22 +19,27 @@ require([], function() {
 	};
 
 	module.getWord = function() {
-		var date = new Date(),
-				today = date.getUTCFullYear() + '-' + date.getMonth() + '-' + date.getDate(),
+		var year = 2015,
+				month = Math.floor( Math.random() * 12 + 1 ),
+				day = Math.floor( Math.random() * 28 + 1 ),
+				date = year + '-' + month + '-' + day,
 				key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-				url = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + today + '&api_key=' + key;
+				url = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=' + date + '&api_key=' + key;
 
-		$.get( url, module.showWord )
+		$.get( url, module.getDefinition )
 			.fail( function() {
 				console.log('could not get a word');
 			});
 	};
 
-	module.showWord = function( data ) {
+	module.getDefinition = function( data ) {
+		console.log( data );
+
 		module.$word.text( data.word );
 		module.$definition.text( data.definitions[0].text );
 		module.$note.text( data.note );
 		module.$example.text( data.examples[0].text );
+		module.$partOfSpeech.text( data.definitions[0].partOfSpeech );
 	};
 
 	module.init();
