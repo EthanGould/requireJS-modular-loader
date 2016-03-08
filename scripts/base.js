bb65a59164cc26dab5515cdb033a3a9aea3cfd0c
EthanGould <ethan.g.907@gmail.com>
2016-03-08 18:48:07 -0500

    added drop shadow to theme color picker

--- scripts/base.js
+++ scripts/base.js
@@ -2,5 +2,3 @@ var $ = require([ './scripts/lib/jquery.min.js' ]);
 require([ './scripts/modules/friendly-jester.js' ]);
 require([ './scripts/modules/wotd.js' ]);
 require([ './scripts/modules/qotd.js' ]);
-
-
